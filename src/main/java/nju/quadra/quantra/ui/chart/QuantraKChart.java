1a51de08777b3b2fdf23cb3c3b7619cc7acfe093
151250091 <151250091@smail.nju.edu.cn>
2017-03-07 01:01:43 +0800

    "The world's # 2 easy-to-use Candlestick chart for JavaFX" #17

--- src/main/java/nju/quadra/quantra/ui/chart/QuantraKChart.java
+++ src/main/java/nju/quadra/quantra/ui/chart/QuantraKChart.java
@@ -1,25 +1,106 @@
 package nju.quadra.quantra.ui.chart;
 
+import javafx.collections.FXCollections;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.Region;
 import javafx.scene.shape.Line;
 import nju.quadra.quantra.data.StockBaseProtos.StockBase.StockInfo;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by adn55 on 2017/3/4.
  */
 public class QuantraKChart extends XYChart<String, Number> {
 
-    public QuantraKChart(Axis<String> xAxis, Axis<Number> yAxis) {
+    private Line horiLine = new Line(0, 0, 0, 0);
+    private Line vertLine = new Line(0, 0, 0, 0);
+    private Region plotBackground = (Region) lookup(".chart-plot-background");
+    private Region plotArea = new Region();
+    private Label toolTip = new Label();
+    private Label yTip = new Label();
+
+    private QuantraKChart(Axis<String> xAxis, Axis<Number> yAxis) {
         super(xAxis, yAxis);
         this.getStylesheets().setAll(getClass().getResource("QuantraKChart.css").toString());
+        // Create lines
+        getPlotChildren().addAll(horiLine, vertLine, plotArea, toolTip);
+        horiLine.getStyleClass().add("line");
+        vertLine.getStyleClass().add("line");
+        // Create tooltip
+        toolTip.getStyleClass().add("tooltip");
+        toolTip.resize(120, 60);
+        toolTip.setMouseTransparent(true);
+        toolTip.setVisible(false);
+        // Create Ytip
+        getChildren().add(yTip);
+        yTip.getStyleClass().add("tooltip");
+        yTip.resize(50, 30);
+        yTip.setVisible(false);
+        // Bind mouse events
+        plotArea.setOnMouseEntered(event -> {
+            horiLine.setVisible(true);
+            vertLine.setVisible(true);
+            toolTip.setVisible(true);
+            yTip.setVisible(true);
+        });
+        plotArea.setOnMouseExited(event -> {
+            horiLine.setVisible(false);
+            vertLine.setVisible(false);
+            toolTip.setVisible(false);
+            yTip.setVisible(false);
+        });
+        plotArea.setOnMouseMoved(event -> {
+            horiLine.relocate(0, event.getY());
+            String xValue = xAxis.getValueForDisplay(event.getX());
+            double yPos = event.getY();
+            yTip.setText(yAxis.getValueForDisplay(yPos).toString());
+            yTip.relocate(5, yPos - 10);
+            if (xValue != null) {
+                Iterator<Data<String, Number>> dataIterator = getDisplayedDataIterator(getData().get(0));
+                while (dataIterator.hasNext()) {
+                    StockInfo info = (StockInfo) dataIterator.next().getExtraValue();
+                    if (info != null && info.getDate().equals(xValue)) {
+                        double xPos = xAxis.getDisplayPosition(xValue);
+                        vertLine.relocate(xPos, 0);
+                        if (xPos + 10 + toolTip.getWidth() > plotBackground.getWidth()) {
+                            xPos -= toolTip.getWidth() + 20;
+                        }
+                        if (yPos + 10 + toolTip.getHeight() > plotBackground.getHeight()) {
+                            yPos -= toolTip.getHeight() + 20;
+                        }
+                        toolTip.relocate(xPos + 10, yPos + 10);
+                        toolTip.setText(xValue + "\n开: " + info.getOpen() + " 收: " + info.getClose() + "\n高: " + info.getHigh() + " 低: " + info.getLow());
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    public static QuantraKChart createFrom(List<StockInfo> infoList) {
+        // Create axis
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setForceZeroInRange(false);
+        // Create series and add data
+        Series<String, Number> series = new Series<>();
+        for (StockInfo info : infoList) {
+            series.getData().add(new Data<>(info.getDate(), info.getOpen(), info));
+        }
+        // Create chart
+        QuantraKChart kChart = new QuantraKChart(xAxis, yAxis);
+        kChart.setData(FXCollections.observableArrayList(series));
+        return kChart;
     }
 
     @Override
@@ -30,6 +111,11 @@ public class QuantraKChart extends XYChart<String, Number> {
             item.setNode(candle);
         }
         getPlotChildren().add(candle);
+        // Make sure that lines are at the front
+        horiLine.toFront();
+        vertLine.toFront();
+        plotArea.toFront();
+        toolTip.toFront();
     }
 
     @Override
@@ -39,7 +125,6 @@ public class QuantraKChart extends XYChart<String, Number> {
 
     @Override
     protected void dataItemChanged(Data<String, Number> item) {
-
     }
 
     @Override
@@ -51,9 +136,25 @@ public class QuantraKChart extends XYChart<String, Number> {
 
     @Override
     protected void seriesRemoved(Series<String, Number> series) {
-        for (Data item : series.getData()) {
+        for (Data<String, Number> item : series.getData()) {
             dataItemRemoved(item, series);
         }
+    }
+
+    @Override
+    protected void updateAxisRange() {
+        super.updateAxisRange();
+        List<Number> yData = new ArrayList<>();
+        for (Series<String, Number> series : getData()) {
+            for (Data item : series.getData()) {
+                StockInfo info = (StockInfo) item.getExtraValue();
+                if (info != null) {
+                    yData.add(info.getLow());
+                    yData.add(info.getHigh());
+                }
+            }
+        }
+        getYAxis().invalidateRange(yData);
     }
 
     @Override
@@ -67,10 +168,9 @@ public class QuantraKChart extends XYChart<String, Number> {
                 Data<String, Number> item = iter.next();
                 double x = getXAxis().getDisplayPosition(getCurrentDisplayedXValue(item));
                 double y = getYAxis().getDisplayPosition(getCurrentDisplayedYValue(item));
-                Node itemNode = item.getNode();
+                Candle candle = (Candle) item.getNode();
                 StockInfo info = (StockInfo) item.getExtraValue();
-                if (itemNode instanceof Candle && info != null) {
-                    Candle candle = (Candle) itemNode;
+                if (candle != null && info != null) {
                     double closePos = getYAxis().getDisplayPosition(info.getClose());
                     double highPos = getYAxis().getDisplayPosition(info.getHigh());
                     double lowPos = getYAxis().getDisplayPosition(info.getLow());
@@ -81,6 +181,10 @@ public class QuantraKChart extends XYChart<String, Number> {
                 }
             }
         }
+        // lines
+        horiLine.setEndX(getWidth());
+        vertLine.setEndY(getHeight());
+        plotArea.resize(getWidth(), getHeight());
     }
 
     class Candle extends Group {
@@ -93,10 +197,9 @@ public class QuantraKChart extends XYChart<String, Number> {
             setAutoSizeChildren(false);
             getChildren().addAll(highLowLine, bar);
             updateStyleClasses();
-            Tooltip.install(bar, tooltip);
         }
 
-        public void update(double closeOffset, double highOffset, double lowOffset, double candleWidth) {
+        void update(double closeOffset, double highOffset, double lowOffset, double candleWidth) {
             openAboveClose = closeOffset > 0;
             updateStyleClasses();
             highLowLine.setStartY(highOffset);
@@ -109,10 +212,6 @@ public class QuantraKChart extends XYChart<String, Number> {
             } else {
                 bar.resizeRelocate(-candleWidth / 2, closeOffset, candleWidth, closeOffset * -1);
             }
-        }
-
-        public void updateTooltip(double open, double close, double high, double low) {
-            tooltip.setText("Open: " + open + "\nClose: " + close + "\nHigh: " + high + "\nLow: " + low);
         }
 
         private void updateStyleClasses() {
--- src/main/java/nju/quadra/quantra/ui/chart/QuantraKChartDemo.java
+++ src/main/java/nju/quadra/quantra/ui/chart/QuantraKChartDemo.java
@@ -1,16 +1,12 @@
 package nju.quadra.quantra.ui.chart;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
+import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 import nju.quadra.quantra.data.StockBaseProtos.StockBase.StockInfo;
 import nju.quadra.quantra.data.StockData;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,24 +17,15 @@ public class QuantraKChartDemo extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Graph Test");
-        List<StockInfo> infoList = StockData.getList();
-        Stream<StockInfo> stream = infoList.stream().filter(info -> info.getDate().startsWith("3/") && info.getDate().endsWith("/14") && info.getCode() == 1);
-
-        CategoryAxis xAxis = new CategoryAxis();
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setForceZeroInRange(false);
-        QuantraKChart kChart = new QuantraKChart(xAxis, yAxis);
-        kChart.setTitle("深发展A 2014/03");
-
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
-        for (XYChart.Data<String, Number> data : stream.map(info -> new XYChart.Data<String, Number>(info.getDate(), info.getOpen(), info)).collect(Collectors.toList())) {
-            series.getData().add(0, data);
-        }
-        kChart.setData(FXCollections.observableArrayList(series));
-
-        stage.setScene(new Scene(kChart, 800, 450));
-        stage.show();
+        new Thread(() -> {
+            Stream<StockInfo> stream = StockData.getList().stream().filter(info -> info.getDate().startsWith("3/") && info.getDate().endsWith("/14") && info.getCode() == 1);
+            QuantraKChart kChart = QuantraKChart.createFrom(stream.collect(Collectors.toList()));
+            Platform.runLater(() -> {
+                stage.setTitle("Graph Test");
+                stage.setScene(new Scene(kChart, 800, 450));
+                stage.show();
+            });
+        }).start();
     }
 
     public static void main(String[] args) {
