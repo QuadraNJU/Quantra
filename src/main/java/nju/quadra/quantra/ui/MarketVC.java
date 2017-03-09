4b3a5bb90ee4ff0bb51d81fff99b3063af592bfe
151250091 <151250091@smail.nju.edu.cn>
2017-03-10 02:07:06 +0800

    beautify

--- src/main/java/nju/quadra/quantra/ui/MarketVC.java
+++ src/main/java/nju/quadra/quantra/ui/MarketVC.java
@@ -3,14 +3,11 @@ package nju.quadra.quantra.ui;
 import com.jfoenix.controls.JFXDatePicker;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.geometry.HPos;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Paint;
 import javafx.util.Callback;
 import nju.quadra.quantra.data.StockBaseProtos;
 import nju.quadra.quantra.data.StockData;
@@ -26,8 +23,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static nju.quadra.quantra.utils.DateUtil.localDateToString;
-
 /**
  * Created by RaUkonn on 2017/3/7.
  */
@@ -39,7 +34,6 @@ public class MarketVC extends Pane {
     private MarketMiniListVC fallingOverFivePer = new MarketMiniListVC(false, "下跌超5%");
     private MarketMiniListVC underLastFivePer = new MarketMiniListVC(true, "上期指数超-5%", "指数");
     private MarketMiniListVC overLastFivePer = new MarketMiniListVC(false, "上期指数超5%", "指数");
-    private String currentDate;
 
     @FXML
     private GridPane gridPane, gridTemp;
@@ -50,17 +44,18 @@ public class MarketVC extends Pane {
 
     public MarketVC() throws IOException {
         FXUtil.loadFXML(this, getClass().getResource("assets/market.fxml"));
-        currentDate = StockData.getList().get(0).getDate();
         gridPane.add(risingLimit, 0, 0);
         gridPane.add(fallingLimit, 1, 0);
         gridPane.add(risingOverFivePer, 0, 1);
         gridPane.add(fallingOverFivePer, 1, 1);
         gridPane.add(underLastFivePer, 0, 2);
         gridPane.add(overLastFivePer, 1, 2);
-        labelDate.setText(dateParser(currentDate));
-        loadLists(currentDate);
+        picker.valueProperty().addListener((observable, oldValue, newValue) -> {
+            loadLists(DateUtil.localDateToString(newValue));
+        });
+        picker.setValue(DateUtil.parseLocalDate(StockData.getList().get(0).getDate()));
+        //loadLists(StockData.getList().get(0).getDate());
         picker.setDayCellFactory(dayCellFactory);
-        picker.setValue(DateUtil.parseLocalDate(currentDate));
     }
 
     private String dateParser(String dateFromDataset) {
@@ -117,7 +112,6 @@ public class MarketVC extends Pane {
                         cnts[1]++;
                     }
 
-
                     if (otherRate > 0.05) {
                         stockOverLastFivePer.add(curr);
                         overLastFivePerRate.add(otherRate);
@@ -142,45 +136,22 @@ public class MarketVC extends Pane {
                 overLastFivePer.setListView(stockOverLastFivePer, overLastFivePerRate);
                 underLastFivePer.setListView(stockUnderLastFivePer, underLastFivePerRate);
 
-                Format f = new DecimalFormat(".##");
-                double risingPer = (double) cnts[0] / Arrays.stream(cnts).sum();
-                double balancingPer = (double) cnts[2] / Arrays.stream(cnts).sum();
-                double fallingPer = 1 - risingPer - balancingPer;
-                gridTemp.getChildren().clear();
-                gridTemp.add(getCenterLabel(f.format(risingPer * 100) + '%', "-fx-background-color: #ff0000;"), 0, 0);
-                gridTemp.add(getCenterLabel(f.format(balancingPer * 100) + '%', "-fx-background-color: #ffff00;"), 1, 0);
-                gridTemp.add(getCenterLabel(f.format(fallingPer * 100) + '%', "-fx-background-color: #00ff00;"), 2, 0);
-                gridTemp.getColumnConstraints().setAll(getColumn(risingPer), getColumn(balancingPer), getColumn(fallingPer));
+                labelDate.setText(dateParser(date));
+                Format f = new DecimalFormat("#.##");
+                int sum = Arrays.stream(cnts).sum();
+                String[] tempTitles = {"上涨", "平盘", "下跌"};
+                for (int i = 0; i < 3; i++) {
+                    double pct = ((double) cnts[i] / sum) * 100;
+                    ((Label) gridTemp.getChildren().get(i)).setText(tempTitles[i] + "（" + f.format(pct) + "%）");
+                    if (pct < 15) pct = 15;
+                    gridTemp.getColumnConstraints().get(i).setPercentWidth(pct);
+                }
                 UIContainer.hideLoading();
             });
         }).start();
     }
 
-    private ColumnConstraints getColumn(double precent) {
-        ColumnConstraints constraints = new ColumnConstraints();
-        constraints.setPercentWidth(precent * 100);
-        return constraints;
-    }
-
-    private GridPane getCenterLabel(String text, String styleSheet) {
-        GridPane pane = new GridPane();
-        pane.setStyle(styleSheet);
-        Label label = new Label(text);
-        label.setTextFill(Paint.valueOf("black"));
-        label.setPrefHeight(30);
-        pane.add(label, 0, 0);
-        GridPane.setHalignment(label, HPos.RIGHT);
-        return pane;
-    }
-
-    public void onActionDateChange() {
-        LocalDate date = picker.getValue();
-        currentDate = localDateToString(date);
-        loadLists(currentDate);
-        labelDate.setText(dateParser(currentDate));
-    }
-
-    final Callback<DatePicker, DateCell> dayCellFactory =
+    private final Callback<DatePicker, DateCell> dayCellFactory =
             new Callback<DatePicker, DateCell>() {
                 @Override
                 public DateCell call(final DatePicker datePicker) {
@@ -192,7 +163,7 @@ public class MarketVC extends Pane {
                                     || item.getDayOfWeek() == DayOfWeek.SUNDAY
                                     || item.getDayOfWeek() == DayOfWeek.SATURDAY) {
                                 setDisable(true);
-                                setStyle("-fx-background-color: #ffc0cb;");
+                                setStyle("-fx-background-color: #ffc0cb; -fx-background-radius: 50%;");
                             }
                         }
                     };
