5489c6a7b5723deff4a86ec97665e4b46694d86a
151250091 <151250091@smail.nju.edu.cn>
2017-03-15 22:24:37 +0800

    fxxk JavaFX

--- src/main/java/nju/quadra/quantra/ui/StockListVC.java
+++ src/main/java/nju/quadra/quantra/ui/StockListVC.java
@@ -6,7 +6,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.util.Callback;
 import nju.quadra.quantra.data.StockData;
 import nju.quadra.quantra.data.StockInfoPtr;
@@ -19,7 +19,7 @@ import java.io.IOException;
 /**
  * Created by Lenovo on 2017/3/14.
  */
-public class StockListVC extends Pane {
+public class StockListVC extends BorderPane {
 
     @FXML
     private TableView<StockInfoPtr> table;
@@ -31,12 +31,12 @@ public class StockListVC extends Pane {
 
         addColumn("代码", param -> new ReadOnlyObjectWrapper<>(String.format("%06d", param.getValue().getToday().getCode())));
         addColumn("名称", param -> new ReadOnlyObjectWrapper<>(param.getValue().getToday().getName()));
-        addColumn("涨幅", param -> new ReadOnlyObjectWrapper<>(Math.floor(StockStatisticUtil.RATE(param.getValue()) * 10000) / 100.0));
+        addColumn("涨幅", param -> new ReadOnlyObjectWrapper<>(Math.floor(StockStatisticUtil.RATE(param.getValue()) * 10000) / 100.0 + "%"));
         addColumn("今收", param -> new ReadOnlyObjectWrapper<>(param.getValue().getToday().getClose()));
+        addColumn("交易量", param -> new ReadOnlyObjectWrapper<>(param.getValue().getToday().getVolume()));
         addColumn("今开", param -> new ReadOnlyObjectWrapper<>(param.getValue().getToday().getOpen()));
         addColumn("最高", param -> new ReadOnlyObjectWrapper<>(param.getValue().getToday().getHigh()));
         addColumn("最低", param -> new ReadOnlyObjectWrapper<>(param.getValue().getToday().getLow()));
-        addColumn("交易量", param -> new ReadOnlyObjectWrapper<>(param.getValue().getToday().getLow()));
         addColumn("昨收", param -> new ReadOnlyObjectWrapper<>(param.getValue().getYesterday().getClose()));
 
         datePicker.valueProperty().addListener(observable -> updateInfo());
