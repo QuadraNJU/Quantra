c13be41eb70be1bb6d56bbdb0fa9c618edaf128e
151160027 <151160027@smail.nju.edu.cn>
2017-03-17 15:06:08 +0800

    股票代码搜索

--- src/main/java/nju/quadra/quantra/ui/UIContainer.java
+++ src/main/java/nju/quadra/quantra/ui/UIContainer.java
@@ -1,16 +1,21 @@
 package nju.quadra.quantra.ui;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import nju.quadra.quantra.data.StockData;
+import nju.quadra.quantra.data.StockInfoPtr;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 /**
  * Created by adn55 on 2017/3/8.
@@ -24,6 +29,8 @@ public class UIContainer extends Stage {
     private static StackPane contentPaneS;
     private static Pane loadingPaneS;
     @FXML
+    private JFXTextField searchBox;
+    @FXML
     private VBox paneCompareList;
     public static VBox paneCompareListS;
     private static int loadingCount = 0;
@@ -34,6 +41,25 @@ public class UIContainer extends Stage {
             loader.setController(this);
             this.setScene(new Scene(loader.load()));
             this.setMaximized(true);
+            searchBox.textProperty().addListener((observable, oldValue, newValue) -> {
+                String text = newValue.trim().toLowerCase();
+                if (!text.isEmpty()) {
+                    char first = text.charAt(0);
+                    if (first >= '0' && first <= '9') {
+                        int input = Integer.parseInt(text);
+                        int lower = (int) (input * Math.pow(10, 6 - text.length()));
+                        int upper = (int) ((input + 1) * Math.pow(10, 6 - text.length()) - 1);
+                        for (StockInfoPtr ptr : StockData.getIndex().stream().filter(ptr -> ptr.get().getCode() >= lower && ptr.get().getCode() <= upper).collect(Collectors.toList())) {
+                            System.out.println(ptr.get().getCode() + " " + ptr.get().getName());
+                        }
+                        System.out.println("finished");
+                    } else if (first >= 'a' && first <= 'z') {
+                        
+                    } else {
+
+                    }
+                }
+            });
             contentPaneS = contentPane;
             loadingPaneS = loadingPane;
             paneCompareListS = paneCompareList;
@@ -60,6 +86,12 @@ public class UIContainer extends Stage {
             loadingPaneS.setVisible(false);
             contentPaneS.setVisible(true);
         }
+    }
+
+    @FXML
+    private void onSearchKeyTyped(KeyEvent t) {
+        JFXTextField textField = (JFXTextField) t.getSource();
+
     }
 
     @FXML
