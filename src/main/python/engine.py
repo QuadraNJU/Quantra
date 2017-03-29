26ddd6facfad435245b353af5a5531a4824d829b
151250110 <151250110@smail.nju.edu.cn>
2017-03-30 00:22:09 +0800

    too many kengs

--- src/main/python/engine.py
+++ src/main/python/engine.py
@@ -1,11 +1,44 @@
 # coding=utf-8
 import imp
 import json
-import time
+import random
+import sys
 
+import numpy
 import os
 import pandas
-import sys
+
+
+def generate_fake_base_rate(length):
+    lis = []
+    for k in range(0, length):
+        lis.append(random.uniform(0, 2))
+    return lis
+
+
+# 补充说明（一）说这里用基准收益率，可是基准收益率是一组数据，不能同无风险利率rf直接相减。我总觉得E(Rm)的意思是基准收益率的期望。。。
+def alpha(_annualized_earning_rate, _base_earning_rate, _beta, _risk_free_interest_rate=0.0175):
+    return (_annualized_earning_rate - _risk_free_interest_rate) \
+           - _beta * numpy.floor(numpy.mean(_base_earning_rate) - _risk_free_interest_rate)
+
+
+# 同理，补充说明（一）说这里用策略收益率，但是策略收益率为一组数据。我只能理解成年化后的策略收益率（知乎上也是这么写的）
+# 附知乎：https://www.zhihu.com/question/27264526
+def sharp(_daily_earnings_rate, _annualized_earning_rate, _risk_free_interest_rate=0.0175):
+    return (_annualized_earning_rate - _risk_free_interest_rate) / numpy.std(_daily_earnings_rate)
+
+
+def beta(_daily_earnings_rate, _base_earning_rate):
+    n = len(_base_earning_rate)
+    if (n == 1):
+        return 0
+    cov = 0
+    mean_strategy = numpy.mean(_daily_earnings_rate)
+    mean_base = numpy.mean(_base_earning_rate)
+    for j in range(0, n):
+        cov += (_daily_earnings_rate[j] - mean_strategy) * (_base_earning_rate[j] - mean_base)
+    cov = 1.0 / (n - 1) * cov
+    return cov / numpy.var(_base_earning_rate)
 
 
 def get_column_index(key):
@@ -104,12 +137,20 @@ if __name__ == '__main__':
         account.portfolio = new_portfolio
         earn_rate = (new_portfolio - capital) / capital
         daily_earnings_rate.append(earn_rate)
+        # 这个年化我是乱写的。。。知乎和我的书上的年化方式不一样。。。我也是醉了。。。这里用知乎的
+        annualized = earn_rate / (start_date_index - end_date_index + 1) * 250  # 这里需要求日期跨度，但是由于早的日期在后面，所以用start - end
 
         progress = int((start_date_index - i) * 100.0 / (start_date_index - end_date_index))
         info = {'progress': progress, 'date': trade_days[i], 'cash': account.cash, 'earn_rate': earn_rate}
         print info
         sys.stdout.flush()
 
-    print json.dumps({'success': True, 'progress': 100, 'daily_earnings_rate': daily_earnings_rate})
+    fake_base_rate = generate_fake_base_rate(len(daily_earnings_rate))
+    sharp = sharp(daily_earnings_rate, annualized)
+    beta = beta(daily_earnings_rate, fake_base_rate)
+    alpha = alpha(annualized, fake_base_rate, beta)
+
+    print json.dumps({'success': True, 'progress': 100, 'daily_earnings_rate': daily_earnings_rate,
+                      'annualized_earning_rate': annualized, 'sharp': sharp, 'beta': beta, 'alpha': alpha})
     # print daily_earnings_rate
     # print daily_earnings_rate[-1]
