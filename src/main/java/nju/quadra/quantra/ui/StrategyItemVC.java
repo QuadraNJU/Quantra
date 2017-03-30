19eab13d02a51dcdccc043c63ef50b703217c00b
151250016 <151250016@smail.nju.edu.cn>
2017-03-30 21:22:25 +0800

    策略项目

---
+++ src/main/java/nju/quadra/quantra/ui/StrategyItemVC.java
@@ -0,0 +1,27 @@
+package nju.quadra.quantra.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import nju.quadra.quantra.strategy.AbstractStrategy;
+import nju.quadra.quantra.utils.FXUtil;
+
+import java.io.IOException;
+
+/**
+ * Created by MECHREVO on 2017/3/30.
+ */
+public class StrategyItemVC extends HBox{
+    @FXML
+    private Label labelTitle;
+    @FXML
+    private Label labelSubTitle;
+    private AbstractStrategy strategy;
+
+    public StrategyItemVC(AbstractStrategy strategy) throws IOException {
+        FXUtil.loadFXML(this, getClass().getResource("assets/strategy.fxml"));
+        this.strategy =strategy;
+        labelTitle.setText(strategy.name);
+        labelSubTitle.setText(strategy.getDescription());
+    }
+}
